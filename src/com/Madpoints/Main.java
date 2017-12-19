065e4d35e39ca12298c569e75d517d951a322c96
John Furtado <jf8846@gmail.com>
2017-12-18 19:13:05 -0500

    Char and Boolean

---
+++ src/com/Madpoints/Main.java
@@ -0,0 +1,17 @@
+package com.Madpoints;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // width of 16 (2 bytes)
+	    char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBool = true;
+
+        char registered = '\u00AE';
+
+        System.out.println(registered);
+
+    }
+}
